6724e763a04804a72c7c9273f49f8a208e3e8c9a
EmilSchroder <eschro66@gmail.com>
2018-09-16 21:15:14 +1200

    this is trash, ignore

--- client/components/Backpack.jsx
+++ client/components/Backpack.jsx
@@ -8,25 +8,40 @@ function Backpack(props) {
   // drinkBeers().then(beer => {
   //   beer.map(indiv => console.log(indiv.image))
   // })
- 
+  function startdrag(e){
+    e.dataTransfer.setData("fun", e.target.id)
+}
+
+function allowDrop(e){
+  epreventDefault()
+}
+
+function doTheDrop(e){
+  e.preventDefault();
+  let data = e.dataTransfer.getData("fun")
+  e.target.appendChild(document.getElementById(data))
+  count++;
+  showColor(e)
+  
+}
   
   return (
     <div className="backpack">
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[0]}
+      <div className='backPackSlot' id='1' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b1' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[0]}</div>
       </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[1]}
-      </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[2]}
-      </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[3]}
-      </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[4]}
-      </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[5]}
-      </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[6]}
-      </div>
-      <div className='backPackSlot'>{props.packBeers && props.packBeers[7]}
+      <div className='backPackSlot' id='2' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b2' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[1]}</div>
+      </div><div></div>
+      <div className='backPackSlot' id='3' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b3' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[2]}</div>
+      </div><div></div>
+      <div className='backPackSlot' id='4' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b4' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[3]}</div>
+      </div><div></div>
+      <div className='backPackSlot' id='5' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b5' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[4]}</div>
+      </div><div></div>
+      <div className='backPackSlot' id='6' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b6' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[5]}</div>
+      </div><div></div>
+      <div className='backPackSlot' id='7' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b7' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[6]}</div>
+      </div><div></div>
+      <div className='backPackSlot' id='8' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}><div id='b8' draggable={true} onDragStart={(event)=>startdrag(event)}>{props.packBeers && props.packBeers[7]}</div>
       </div>
 </div>
 )
--- client/components/MixArea.jsx
+++ client/components/MixArea.jsx
@@ -3,11 +3,28 @@ import React from 'react'
 
 
 function MixArea () {
+
+  function startdrag(e){
+    e.dataTransfer.setData("fun", e.target.id)
+}
+
+function allowDrop(e){
+  epreventDefault()
+}
+
+function doTheDrop(e){
+  e.preventDefault();
+  let data = e.dataTransfer.getData("fun")
+  e.target.appendChild(document.getElementById(data))
+  count++;
+  showColor(e)}
+
+
   return (
     <div className="mixArea">
-      <div className='mixSlot'>
+      <div id='m1' className='mixSlot' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}>
       </div>
-      <div className='mixSlot'>
+      <div id='m2' className='mixSlot' onDrag={(event)=>doTheDrop(event)} onDragOver={(event)=>allowDrop(event)}>
       </div>
       <div className='imageDiv'>
       <img id='equalsImg' src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Equals_sign.svg/2000px-Equals_sign.svg.png" alt=""/>
